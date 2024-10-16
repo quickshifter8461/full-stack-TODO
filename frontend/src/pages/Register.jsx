3799f82144485264a35537331a390c75dc72374e
Vishnu P <24x7vishnu@gmail.com>
2024-10-16 21:49:23 +0530

    final update

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -34,9 +34,7 @@ function Register() {
     } catch (err) {
       if (err.response && err.response.data && err.response.data.message) {
         setError(err.response.data.message);
-      } else {
-        setError("Registration failed");
-      }
+      } 
     }
   };
 
